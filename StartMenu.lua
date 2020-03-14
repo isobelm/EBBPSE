350d4cd95419f0d5f7a149d854d9841118acb315
isobelm <isobel.mahon@gmail.com>
2020-03-14 20:06:29 +0000

    start menu

--- StartMenu.lua
+++ StartMenu.lua
@@ -4,8 +4,8 @@ local StartMenu = {}
 StartMenu.__index = StartMenu
 
 StartMenu.background = love.graphics.newImage('Resources/Backgrounds/interaction.png')
-StartMenu.dialogueOption = love.graphics.newImage('Resources/Images/dialogueOption.png')
-StartMenu.selectedDialogueOption = love.graphics.newImage('Resources/Images/selectedDialogueOption.png')
+StartMenu.menuOption = love.graphics.newImage('Resources/Images/menuOption.png')
+StartMenu.selectedMenuOption = love.graphics.newImage('Resources/Images/selectedMenuOption.png')
 
 function StartMenu:init()
 	self.image = love.graphics.newImage('Resources/Backgrounds/StartMenu.png')
@@ -17,21 +17,21 @@ end
 function StartMenu:draw()
 	if self.dirty then
 		love.graphics.draw(self.image, 0, 0)
-		for i=1,length do
-			self:drawOption(self.optionText[i], i, self.selected == i)
+		for i=1, table.getn(self.options) do
+			self:drawOption(self.options[i], i, self.selected == i)
 		end
-		love.graphics.printf("Press any key to start", 0, 250, 800, 'center')
+		-- love.graphics.printf("Press any key to start", 0, 250, 800, 'center')
 		self.dirty = false
 	end
 end
 
-function Interaction:drawOption(text, pos, isSelected)
+function StartMenu:drawOption(text, pos, isSelected)
 	if isSelected then
-		love.graphics.draw(StartMenu.selectedDialogueOption, 48, 356 + pos * 56)
+		love.graphics.draw(StartMenu.selectedMenuOption, 224, 169 + pos * 65)
 	else
-		love.graphics.draw(StartMenu.dialogueOption, 48, 356 + pos * 56)
+		love.graphics.draw(StartMenu.menuOption, 224, 169 + pos * 65)
 	end
-	love.graphics.printf(text, 64, 368 + pos * 56, 672, 'left')
+	love.graphics.printf(text, 64, 169 + 15 + pos * 65, 672, 'center')
 end
 
 function StartMenu:newScreen()
@@ -45,7 +45,18 @@ function StartMenu:update(dt)
 end
 
 function StartMenu:keyreleased( key )
-	return "Levels/TownGate"
+		if key == "up" then
+			if (self.selected > 1) then
+				self.selected = self.selected - 1
+			end
+		elseif key == "down" then
+			if (self.selected < table.getn(self.options)) then
+				self.selected = self.selected + 1
+			end
+		elseif key == "space" or key == "return" then
+			return "Levels/TownGate"
+		end
+	self.dirty = true;
 end
 
 function StartMenu:keypressed(key)
